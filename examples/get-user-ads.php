4e884601d37f5543a14fd7bc3900145a7389cc5c
Jaroslaw Wieczorek <jaroslaw.wieczorek@otodom.pl>
2016-10-25 13:47:38 +0200

    openapi sdk work in progress

--- examples/get-user-ads.php
+++ examples/get-user-ads.php
@@ -5,34 +5,31 @@
 
 require 'init.php';
 
+$api = new naspersclassifieds\realestate\openapi\OpenApi(OPENAPI_URL);
+
 echo "Logging in...\n";
-$client = createClient();
-$accessToken = logIn($client);
+$api->logIn(OPENAPI_KEY, OPENAPI_SECRET, OTODOM_USER, OTODOM_PASSWORD);
 echo "Logged in.\n";
 
 echo "Reading first 20 ads ordered by creation time...\n";
-$response = $client->request(
-    'GET',
-    "account/adverts",
-    [
-        'query' => [
-            'access_token' => $accessToken,
-            'limit' => 20,
-            'sortby' => 'created_at_first',
-            'sortdirection' => 'desc'
-        ]
-    ]
-);
-$body = json_decode($response->getBody()->getContents(), true);
 
-if (empty($body['results'])) {
+$query = (new naspersclassifieds\realestate\openapi\query\AccountAdvertsQuery)
+    ->setLimit(20)
+    ->setSortBy(naspersclassifieds\realestate\openapi\query\AccountAdvertsQuery::SORT_BY_CREATED_AT)
+    ->setSortDirection(naspersclassifieds\realestate\openapi\query\AccountAdvertsQuery::SORT_ASC)
+;
+
+$results = $api->getAccount()->getAdverts($query);
+
+if (empty($results->results)) {
     echo "No ads. Please add some ads using the create-ad script or via the webpage.\n";
     exit;
 }
-echo "Showing page {$body['current_page']} of {$body['total_pages']} containing {$body['current_elements']} out of {$body['total_elements']} ads:\n";
+echo "Showing page {$results->current_page} of {$results->total_pages}" .
+    " containing {$results->current_elements} out of {$results->total_elements} ads:\n";
 
 echo sprintf('%-10s | %-15s | %s', 'ID', 'Status', 'Title') . "\n";
 echo str_repeat('-', 75) . "\n";
-foreach ($body['results'] as $ad) {
-    echo sprintf('%10d | %-15s | %s', $ad['id'], $ad['status'], $ad['title']) . "\n";
+foreach ($results->results as $ad) {
+    echo sprintf('%10d | %-15s | %s', $ad->id, $ad->status, $ad->title) . "\n";
 }
