f8ce3f85dbcadf0a0e37209888177610acfb384c
Daniel Farbiszewski <daniel.farbiszewski@otodom.pl>
2016-10-27 12:46:36 +0200

    manage agents

--- tests/utils/Constants.php
+++ tests/utils/Constants.php
@@ -12,9 +12,4 @@ class Constants
     const USER_LOGIN = "test@dummy.address";
     const USER_PASSWORD = "validpassword";
     const AUTH_TOKEN = "0e537913c8cad357012202ef6cccc9558717ddc2";
-    const AGENT_NAME = "Imi Nazwonko";
-    const AGENT_PHONE = "555 666 777";
-    const AGENT_ID = 2199610;
-    const AGENT_PHOTO = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAnUlEQVR42mL4//8/AwgDgT0QbwTiJ1AMYtvD5aGKKoD4Pw5cAVUDNukPEG8HYksgZoZiS6gYSM6GEUgcBuKPQF0+DFgAIyPjaiAlCmJ/AGJdIN4Lwkhu3gvFILkPLFCNN4H4F5phv5DkQE4BW60LMwkdQ008APPMOiSJh0B8CYm/AuQZ5OBZB/IpkgJLqFgFPBzRAvw5FKMEOECAAQA6DXcHpH9ICAAAAABJRU5ErkJggg==';
-    const AGENT_EMAIL = 'kulamula@hula.ho';
 }
